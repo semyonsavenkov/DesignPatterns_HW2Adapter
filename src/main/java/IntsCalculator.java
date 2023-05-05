0ee96e702abc9adb8139273e3822a069e29563dc
semyonsavenkov <semyonsavenkov@gmail.com>
2023-05-05 23:25:25 +0300

    Adapter done

--- src/main/java/IntsCalculator.java
+++ src/main/java/IntsCalculator.java
@@ -8,16 +8,31 @@ public class IntsCalculator implements Ints {
 
     @Override
     public int sum(int arg0, int arg1) {
-        return 0;
+        double result = target.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.SUM)
+                .result();
+        return (int) result;
     }
 
     @Override
     public int mult(int arg0, int arg1) {
-        return 0;
+        double result = target.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.MULT)
+                .result();
+        return (int) result;
     }
 
     @Override
     public int pow(int a, int b) {
-        return 0;
+        double result = target.newFormula()
+                .addOperand(a)
+                .addOperand(b)
+                .calculate(Calculator.Operation.POW)
+                .result();
+        return (int) result;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Ints customCalc = new IntsCalculator();
+        System.out.println(customCalc.sum(10, 5));
+        System.out.println(customCalc.mult(10, 5));
+        System.out.println(customCalc.pow(10, 5));
     }
 
 }
